2cd6a69ae1573199897591cbae6c559e0047b624
Suraj Gaur <surajgaur242@gmail.com>
2024-10-12 21:40:55 +0530

    This commit is improve some code

--- loade_more_data/index.php
+++ loade_more_data/index.php
@@ -1,12 +1,13 @@
 <?php
 
-include"dbconnection.php";
+// include"dbconnection.php";
 
-$query = "select * from my_tbl order by id limit 2";
-$run = mysqli_query($conn, $query);
+// $query = "select * from my_tbl order by id limit 2";
+// $run = mysqli_query($conn, $query);
 
 ?>
-<!DOCTYPE html>
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -30,14 +31,14 @@ $run = mysqli_query($conn, $query);
             </tr>
             <?php
             
-            while($row = mysqli_fetch_array($run)){
-                echo"<tr>
-                  <td>".$row[0]."</td>
-                  <td>".$row[1]."</td>
-                  <td>".$row[2]."</td>
-                </tr>";
-                $id = $row[0];
-            }
+            // while($row = mysqli_fetch_array($run)){
+            //     echo"<tr>
+            //       <td>".$row[0]."</td>
+            //       <td>".$row[1]."</td>
+            //       <td>".$row[2]."</td>
+            //     </tr>";
+            //     $id = $row[0];
+            // }
             ?>
             <tr id="remove_row">
                 <td colspan="3">
@@ -69,6 +70,96 @@ $run = mysqli_query($conn, $query);
                 })
             })
         })
+    </script>
+</body>
+</html> -->
+
+
+
+
+
+
+
+
+<?php
+include "dbconnection.php";
+
+$query = "SELECT * FROM my_tbl ORDER BY id LIMIT 2";
+$run = mysqli_query($conn, $query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Load More Data Page</title>
+    <style>
+        #loader {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div style="text-align: center;">
+        <h1>This page is an example of loading more data</h1>
+
+        <table border="1" id="data_table" align="center">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Surname</th>
+            </tr>
+            <?php
+            $lastId = null; // Track the last ID
+
+            while ($row = mysqli_fetch_array($run)) {
+                echo "<tr>
+                        <td>{$row[0]}</td>
+                        <td>{$row[1]}</td>
+                        <td>{$row[2]}</td>
+                      </tr>";
+                $lastId = $row[0]; // Update the last ID
+            }
+            ?>
+            <?php if ($lastId !== null): ?>
+            <tr id="remove_row">
+                <td colspan="3">
+                    <input id="load_more" type="button" value="Load more" data-id="<?php echo $lastId; ?>" style="border-style: none; background-color: inherit;">
+                </td>
+            </tr>
+            <?php endif; ?>
+        </table>
+
+        <div id="loader" style="text-align: center;">
+            <img src="loading2.gif" alt="" height="100" width="150">
+        </div>
+    </div>
+
+    <script src="jquery.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(document).on("click", "#load_more", function(event){
+                var id = $(this).attr("data-id"); // Use 'this' to refer to the clicked button
+                $.ajax({
+                    url: "load.php",
+                    type: "POST",
+                    data: { id: id },
+                    beforeSend: function() {
+                        $('#loader').show();
+                    },
+                    success: function(response) {
+                        $("#remove_row").remove();
+                        $("#data_table").append(response);
+                        $('#loader').hide();
+                    },
+                    error: function() {
+                        alert("An error occurred while loading data.");
+                        $('#loader').hide();
+                    }
+                });
+            });
+        });
     </script>
 </body>
 </html>
--- loade_more_data/load.php
+++ loade_more_data/load.php
@@ -1,36 +1,86 @@
 <?php
+// include("dbconnection.php");
+
+// function GetMaxId(){
+//     global $conn;
+//     $query = "SELECT MAX(id) FROM my_tbl";
+//     $run = mysqli_query($conn,$query);
+//     $row = mysqli_fetch_array($run);
+//     return $row[0];
+// }
+
+// if(isset($_POST['id'])){
+//   $id = isset($_POST['id']);
+//   $query = "SELECT * FROM my_tbl WHERE id > '$id' ORDER BY id LIMIT 2";
+//   $runn = mysqli_query($conn,$query);
+//   $output = "";
+//   $newId = null;
+//   while($row = mysqli_fetch_array($runn)){
+//     $output .="<tr>
+//                   <td>".$row[0]."</td>
+//                   <td>".$row[1]."</td>
+//                   <td>".$row[2]."</td>
+//                 </tr>";
+//                 $newId = $row[0];
+                
+//   }
+//   // $newId < GetMaxId()
+//   if($newId !== null && $newId < GetMaxId()){
+//     $output .= "<tr id='remove_row'>
+//                 <td colspan='3'>
+//                     <input id='load_more' type='button' value='load more' data-id=' $id' style='border-style: none; background-color: inherit;'>
+//                 </td>
+//             </tr>";
+//   }
+  
+// }
+// // sleep(2);
+// echo $output;
+?>
+
+
+
+
+
+<?php
 include("dbconnection.php");
 
-function GetMaxId(){
+function GetMaxId() {
     global $conn;
-    $query = "select max(id) from my_tbl";
-    $run = mysqli_query($conn,$query);
+    $query = "SELECT MAX(id) FROM my_tbl";
+    $run = mysqli_query($conn, $query);
     $row = mysqli_fetch_array($run);
     return $row[0];
 }
 
-if(isset($_POST['id'])){
-  $id = isset($_POST['id']);
-  $query = "select * from my_tbl where id > '$id' order by id limit 2";
-  $runn = mysqli_query($conn,$query);
-  $output = "";
-  while($row = mysqli_fetch_array($runn)){
-    $output .="<tr>
-                  <td>".$row[0]."</td>
-                  <td>".$row[1]."</td>
-                  <td>".$row[2]."</td>
+if (isset($_POST['id'])) {
+    $id = intval($_POST['id']); 
+    $query = "SELECT * FROM my_tbl WHERE id > '$id' ORDER BY id LIMIT 2";
+    $runn = mysqli_query($conn, $query);
+    
+    $output = "";
+    $newId = null; 
+
+    while ($row = mysqli_fetch_array($runn)) {
+        $output .= "<tr>
+                      <td>{$row[0]}</td>
+                      <td>{$row[1]}</td>
+                      <td>{$row[2]}</td>
+                    </tr>";
+        $newId = $row[0]; 
+    }
+
+    
+    if ($newId !== null && $newId < GetMaxId()) {
+        $output .= "<tr id='remove_row'>
+                    <td colspan='3'>
+                        <input id='load_more' type='button' value='Load more' data-id='$newId' style='border-style: none; background-color: inherit;'>
+                    </td>
                 </tr>";
-                $id = $row[0];
-  }
-  if($id < GetMaxId()){
-    $output .= "<tr id='remove_row'>
-                <td colspan='3'>
-                    <input id='load_more' type='button' value='load more' data-id=' $id' style='border-style: none; background-color: inherit;'>
-                </td>
-            </tr>";
-  }
-  
+    }
 }
-sleep(2);
+
+// Uncomment this line if you want to simulate a delay
+// sleep(2);
 echo $output;
 ?>
